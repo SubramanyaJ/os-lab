ab91c6d1f979d9f3dab189096c0064dfa670ce2f
Subramanya J <149412686+SubramanyaJ@users.noreply.github.com>
2025-04-03 13:01:53 +0530

    update queue.h

--- 02_multi_level_queue/queue.h
+++ 02_multi_level_queue/queue.h
@@ -1,32 +1,24 @@
 #ifndef _QUEUE_H_
 #define _QUEUE_H_
+
 #include <stdbool.h>
 
 struct Queue {
-	void **arr;
-	int capacity;
-	int items;
-	int front, back;
+    void **arr;
+    int capacity;
+    int items;
+    int front, back;
 };
 
 struct Queue *create_queue(int size);
-
 bool isempty_queue(struct Queue *queue);
-
 bool isfull_queue(struct Queue *queue);
-
 void enqueue_queue(struct Queue *queue, void *val);
-
 void* dequeue_queue(struct Queue *queue);
-
-void display_queue(struct Queue *queue, void* offset);
-
+void display_queue(struct Queue *queue, void (*print_func)(void*));
 void* backof_queue(struct Queue *queue);
-
 void* frontof_queue(struct Queue *queue);
-
 void free_queue(struct Queue *queue);
-
 int itemsin_queue(struct Queue *queue);
 
-#endif /* queue.h included */
+#endif /* queue.h */
